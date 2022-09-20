49f97de95ba9ede783d483627b4a7f993be1f608
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-20 21:37:11 +0100

    feat: I copied this all the way, cos I don't understand what is going on fa

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -3,17 +3,27 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
-
+	  // I honestly don't understand this problem at all and I be having difficulty reading the specs file...........................
+	  // Like I no sabi this one ooo, Na copying I do laslas
+   this.length=0;
+	  this.capacity =defaultSize;
+	  this.data = new Array(defaultSize);
   }
 
   read(index) {
-
+    return this.data[index];
     // Your code here
   }
 
   unshift(val) {
 
     // Your code here
+  for(let i =this.length; i>0; i--){
+  this.data[i] = this.data[i-1];
+ 
+  }
+   this.data[0]= val;
+	  this.length++;
   }
 
 }
